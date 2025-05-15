8e00c9b082311054524060cb6a23f75e756add5f
Rodney Thomas <wj.rodneyt@yahoo.com>
2025-05-15 11:23:02 -0500

    wj

---
+++ wj.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+class WJ:
+
+  def __init__(self):
+    root = tk.Tk()
+    root.title("WARJORN")
+    root.geometry("400x300")
+    root.resizable(False, False)
+    root.configure(bg="grey")
+    root.mainloop()
